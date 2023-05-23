a9ac985175563759e59d42e343d06cd5982cae73
Sathvik Narala <sathviknarala2003@gmail.com>
2023-05-23 23:30:25 +0530

    Validated Inputs

--- myapp/lib/ui.dart
+++ myapp/lib/ui.dart
@@ -2,6 +2,8 @@ import "package:flutter/material.dart";
 import "logic.dart";
 
 class BMI extends State{
+  
+  final _key = GlobalKey<FormState>();
 
   final _height = TextEditingController();
   final _weight = TextEditingController();
@@ -16,76 +18,88 @@ class BMI extends State{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Center(
-            child: Text("BMI Calculator"),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Center(
+          child: Text("BMI Calculator"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Center(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Center(
+          child: Form(
+            key: _key,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Height (in cms)',
-                    labelStyle: TextStyle(
-                      fontSize: 20
-                    ),
-                    floatingLabelAlignment: FloatingLabelAlignment.start
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Height (in cms)',
+                  labelStyle: TextStyle(
+                    fontSize: 20
                   ),
-                  cursorHeight: 25.0,
-                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                  controller: _height,
+                  floatingLabelAlignment: FloatingLabelAlignment.start
                 ),
+                validator: (value) {
+                  return (value == null || value.isEmpty) ? 'Numeric Value Required' : null;
+                },
+                cursorHeight: 25.0,
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                controller: _height,
+              ),
 
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Weight (in kgs)',
-                    labelStyle: TextStyle(
-                      fontSize: 20
-                    ),
-                    floatingLabelAlignment: FloatingLabelAlignment.start
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Weight (in kgs)',
+                  labelStyle: TextStyle(
+                    fontSize: 20
                   ),
-                  cursorHeight: 25.0,
-                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                  controller: _weight,
+                  floatingLabelAlignment: FloatingLabelAlignment.start
+                  
                 ),
+                validator: (value) {
+                  return (value == null || value.isEmpty) ? 'Numeric Value Required' : null;
+                },
+                cursorHeight: 25.0,
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                controller: _weight,
+              ),
 
-                ElevatedButton(
-                  onPressed: (){
-                    setState(()=> _bmi = Logic.compute(
-                      height: double.parse(_height.text),
-                      weight: double.parse(_weight.text)
-                    ));
-                  }, 
-                  child: const Text('Calculate',
-                    textScaleFactor: 1.5,
-                  )
-                ),
-                
-                Text('${_bmi.value.toStringAsFixed(1)} BMI',
-                  textScaleFactor: 4.5,
-                  style: const TextStyle(
-                    color: Colors.red
-                  ),
-                ),
-
-                Text(_bmi.msg,
-                  textScaleFactor: 3.5,
-                  style: const TextStyle(
-                    color: Colors.blue
-                  ),
+              ElevatedButton(
+                onPressed: (){
+                  setState((){
+                    if(_key.currentState!.validate()){
+                      _bmi = Logic.compute(
+                        height: double.parse(_height.text),
+                        weight: double.parse(_weight.text)
+                      );
+                    }
+                  });
+                }, 
+                child: const Text('Calculate',
+                  textScaleFactor: 1.5,
                 )
+              ),
+              
+              Text('${_bmi.value.toStringAsFixed(1)} BMI',
+                textScaleFactor: 4.5,
+                style: const TextStyle(
+                  color: Colors.red
+                ),
+              ),
 
-              ],
-            ),
-          )
-        ),
+              Text(_bmi.msg,
+                textScaleFactor: 3.5,
+                style: const TextStyle(
+                  color: Colors.blue
+                ),
+              )
+
+            ],
+          ),
+        )
+      ),
       )
     );
   }
