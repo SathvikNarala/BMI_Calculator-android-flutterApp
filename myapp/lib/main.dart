f2a2c6c6f183cdebf6e3a58aabe26316e2baf06e
Sathvik Narala <sathviknarala2003@gmail.com>
2023-05-24 10:25:28 +0530

    Second commit

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -3,10 +3,19 @@ import "ui.dart";
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
-  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: _App(),
+    );
+  }
+
+}
+
+class _App extends StatefulWidget{
   @override
   State<StatefulWidget> createState() => BMI();
-
 }
