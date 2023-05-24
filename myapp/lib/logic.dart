9bb9d9185b3af29cb710ffb7055c2520436a9968
Sathvik Narala <sathviknarala2003@gmail.com>
2023-05-24 10:55:54 +0530

    Updated Logic & UI

--- myapp/lib/logic.dart
+++ myapp/lib/logic.dart
@@ -1,6 +1,26 @@
+class Data{
+  double value;
+  String msg;
+
+  Data({this.value = 0.0, this.msg = 'UnderWeight'});
+}
+
 class Logic{
-  static double compute({required double height, required double weight}){
+  static Data compute({required double height, required double weight}){
     height = height / 100;
-    return weight / (height * height);
+    double v = weight / (height * height);
+    String x = '';
+
+    if(v <= 18.5){
+      x = 'UnderWeight';
+    }else if(v > 18.5 && v <= 25) {
+      x = 'Healthy';
+    } else if(v > 25 && v <= 35) {
+      x = 'OverWeight';
+    } else{
+      x = 'Obesity';
+    }
+
+    return Data(value: v, msg: x);
   }
 }
--- myapp/lib/ui.dart
+++ myapp/lib/ui.dart
@@ -2,10 +2,12 @@ import "package:flutter/material.dart";
 import "logic.dart";
 
 class BMI extends State{
+  
+  final _key = GlobalKey<FormState>();
 
   final _height = TextEditingController();
   final _weight = TextEditingController();
-  double _bmi = 0.0;
+  Data _bmi = Data();
 
   @override
   void dispose(){
@@ -16,86 +18,90 @@ class BMI extends State{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Center(
-            child: Text("BMI Calculator"),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Center(
+          child: Text("BMI Calculator"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Center(
+          child: Form(
+            key: _key,
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                constraints: const BoxConstraints.expand(
-                  width: 400,
-                  height: 400
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Height (in cms)',
+                  labelStyle: TextStyle(
+                    fontSize: 20
+                  ),
+                  floatingLabelAlignment: FloatingLabelAlignment.start
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    TextField(
-                      decoration: const InputDecoration(
-                        hintText: "Height (in cms)"
-                      ),
-                      textAlign: TextAlign.center,
-                      cursorHeight: 22.0,
-                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                      controller: _height,
-                    ),
+                validator: (value) {
+                  return (value == null || value.isEmpty) ? 'Numeric Value Required' : null;
+                },
+                cursorHeight: 25.0,
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                controller: _height,
+              ),
 
-                    TextField(
-                      decoration: const InputDecoration(
-                        hintText: "Weight (in kgs)"
-                      ),
-                      textAlign: TextAlign.center,
-                      cursorHeight: 22.0,
-                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                      controller: _weight,
-                    ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Weight (in kgs)',
+                  labelStyle: TextStyle(
+                    fontSize: 20
+                  ),
+                  floatingLabelAlignment: FloatingLabelAlignment.start
+                  
+                ),
+                validator: (value) {
+                  return (value == null || value.isEmpty) ? 'Numeric Value Required' : null;
+                },
+                cursorHeight: 25.0,
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                controller: _weight,
+              ),
 
-                    ElevatedButton(
-                      onPressed: (){
-                        setState(()=> _bmi = Logic.compute(
-                          height: double.parse(_height.text),
-                          weight: double.parse(_weight.text)
-                        ));
-                      }, 
-                      child: const Text('Calculate')
-                    ),
-                  ],
+              ElevatedButton(
+                onPressed: (){
+                  setState((){
+                    if(_key.currentState!.validate()){
+                      _bmi = Logic.compute(
+                        height: double.parse(_height.text),
+                        weight: double.parse(_weight.text)
+                      );
+                    }
+                  });
+                }, 
+                child: const Text('Calculate',
+                  textScaleFactor: 1.5,
+                )
+              ),
+              
+              Text('${_bmi.value.toStringAsFixed(1)} BMI',
+                textScaleFactor: 4.5,
+                style: const TextStyle(
+                  color: Colors.red
                 ),
               ),
 
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Text(_bmi.toStringAsFixed(1),
-                      textScaleFactor: 5.0,
-                      style: const TextStyle(
-                        color: Colors.red
-                      ),
-                    ),
-
-                    const Text('BMI',
-                      textScaleFactor: 3.0,
-                      style: TextStyle(
-                        color: Colors.blue
-                      ),
-                    )
-                  ],
+              Text(_bmi.msg,
+                textScaleFactor: 3.5,
+                style: const TextStyle(
+                  color: Colors.blue
                 ),
               )
+
             ],
           ),
-        ),
-        resizeToAvoidBottomInset: false,
+        )
       ),
-      debugShowCheckedModeBanner: false,
+      )
     );
   }
-
 }
+
